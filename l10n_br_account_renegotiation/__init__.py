0c09bf75791335bf48cd919c1d389ca7521f0ae3
Raphaël Valyi <rvalyi@akretion.com>
2026-01-22 12:44:33 +0000

    add l10n_br_account_renegotiation opus

---
+++ l10n_br_account_renegotiation/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2025 Akretion - Raphael Valyi <raphael.valyi@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+from . import wizards
