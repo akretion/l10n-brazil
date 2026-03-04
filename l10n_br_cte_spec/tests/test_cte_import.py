b15c7fc91548b6724d59bd184794a75829ec6955
Magno Costa <magno.costa@akretion.com.br>
2026-03-04 14:29:18 -0300

    [FIX] l10n_br_cte_spec: odoo_test_helper update

--- l10n_br_cte_spec/tests/test_cte_import.py
+++ l10n_br_cte_spec/tests/test_cte_import.py
@@ -13,9 +13,8 @@ from odoo_test_helper import FakeModelLoader
 from odoo import Command, api, models
 from odoo.tests import TransactionCase
 
-from odoo.addons.l10n_br_cte_spec.models.v4_0 import cte_tipos_basico_v4_00
-
 from ..models import spec_mixin
+from ..models.v4_0 import cte_tipos_basico_v4_00
 
 tz_datetime = re.compile(r".*[-+]0[0-9]:00$")
 
@@ -122,13 +121,12 @@ spec_mixin.CteSpecMixin.build_attrs_fake = build_attrs_fake
 spec_mixin.CteSpecMixin.match_or_create_m2o_fake = match_or_create_m2o_fake
 
 
-class NFeImportTest(TransactionCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.loader = FakeModelLoader(cls.env, cls.__module__)
-        cls.loader.backup_registry()
+class CTeImportTest(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.env = self.env(context=dict(self.env.context, tracking_disable=True))
+        self.loader = FakeModelLoader(self.env, self.__module__)
+        self.loader.backup_registry()
 
         # Get all classes from the module that inherit from AbstractModel
         modified_classes = []
@@ -142,12 +140,8 @@ class NFeImportTest(TransactionCase):
 
                 # Replace original class in module
                 modified_classes.append(modified_class)
-                cls.loader.update_registry(modified_classes)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.loader.restore_registry()
-        super().tearDownClass()
+                self.loader.update_registry(modified_classes)
+                self.addCleanup(self.loader.restore_registry)
 
     def test_import_cte(self):
         file = (
