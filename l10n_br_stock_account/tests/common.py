35a94bcb25aeeb6a6cbaf71bf0a3c2684c436c51
Raphaël Valyi <rvalyi@akretion.com>
2026-06-04 18:25:43 -0300

    [MIG] l10n_br_stock_account: Migration to 18.0

--- l10n_br_stock_account/tests/common.py
+++ l10n_br_stock_account/tests/common.py
@@ -1,15 +1,26 @@
 # @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.stock_picking_invoicing.tests.common import TestPickingInvoicingCommon
+from odoo.addons.stock_picking_invoicing.tests.common import (
+    TestStockPickingInvoicingCommon,
+)
+from odoo.addons.stock_picking_invoicing.tests.tools import (
+    create_with_form_inv_onshipping,
+    create_with_form_pck_backorder,
+    create_with_form_return_picking,
+)
 
 
-class TestBrPickingInvoicingCommon(TestPickingInvoicingCommon):
+class TestBrPickingInvoicingCommon(TestStockPickingInvoicingCommon):
     def _change_user_company(self, company):
         self.env.user.company_ids += company
         self.env.user.company_id = company
 
-    def _run_line_onchanges(self, record):
-        result = super()._run_line_onchanges(record)
-        record._onchange_product_quantity()
-        return result
+    def create_invoice_wizard(self, pickings):
+        return create_with_form_inv_onshipping(self.env, pickings)
+
+    def return_picking_wizard(self, picking):
+        return create_with_form_return_picking(self.env, picking)
+
+    def create_backorder_wizard(self, picking):
+        return create_with_form_pck_backorder(self.env, picking)
