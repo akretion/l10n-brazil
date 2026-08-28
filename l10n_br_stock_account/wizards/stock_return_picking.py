a862a8211f03192a1fc5fe63cd0f0c028868a4b8
Magno Costa <magno.costa@akretion.com.br>
2026-08-28 18:06:00 -0300

    [FIX] l10n_br_stock_account: Method renamed

--- l10n_br_stock_account/wizards/stock_return_picking.py
+++ l10n_br_stock_account/wizards/stock_return_picking.py
@@ -8,16 +8,13 @@ from odoo.exceptions import UserError
 class StockReturnPicking(models.TransientModel):
     _inherit = "stock.return.picking"
 
-    def _create_returns(self):
+    def _create_return(self):
         """
         Creates return picking.
         @param self: The object pointer.
         @return: A dictionary which of fields with values.
         """
-        new_picking_id, pick_type_id = super()._create_returns()
-
-        picking_obj = self.env["stock.picking"]
-        picking = picking_obj.browse(new_picking_id)
+        new_picking = super()._create_return()
 
         origin_picking = self.env["stock.picking"].browse(self.env.context["active_id"])
 
@@ -35,8 +32,8 @@ class StockReturnPicking(models.TransientModel):
                         )
                     )
             else:
-                picking.fiscal_operation_id = refund_fiscal_operation.id
-                for move in picking.move_ids:
+                new_picking.fiscal_operation_id = refund_fiscal_operation
+                for move in new_picking.move_ids:
                     ret_move = move.origin_returned_move_id
                     fiscal_op = ret_move.fiscal_operation_id.return_fiscal_operation_id
                     fiscal_op_line = ret_move.fiscal_operation_line_id.line_refund_id
@@ -49,4 +46,4 @@ class StockReturnPicking(models.TransientModel):
                         vals["fiscal_operation_line_id"] = fiscal_op_line.id
                     if vals:
                         move.write(vals)
-        return new_picking_id, pick_type_id
+        return new_picking
